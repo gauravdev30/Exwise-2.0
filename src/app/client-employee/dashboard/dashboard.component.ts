30a7855505d26b8545f8a3f87b02033e8fd13ee8
gauravdev30 <gauravdagdu@gmail.com>
2024-04-30 01:07:24 +0530

    client survey response reminders,client dashboard

--- src/app/client-employee/dashboard/dashboard.component.ts
+++ src/app/client-employee/dashboard/dashboard.component.ts
@@ -6,9 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  total:any=0;
+  attempted:any=0;
+  notAttempted:any=0;
   items: any[] = [
-    {surveyId:'1', surveyName:'Name 1', status: 'Attempted' },
-    {surveyId:'2', surveyName:'Name 2', status: 'Not attempted' },
-    {surveyId:'3', surveyName:'Name 3', status: 'Attempted' }
+    {surveyId:'1', surveyName:'Name 1', assignedon:'29-04-24', status: 'Attempted' },
+    {surveyId:'2', surveyName:'Name 2', assignedon:'29-04-24', status: 'Not attempted' },
+    {surveyId:'3', surveyName:'Name 3', assignedon:'29-04-24', status: 'Attempted' },
+    {surveyId:'4', surveyName:'Name 4', assignedon:'29-04-24', status: 'Not attempted' },
+    {surveyId:'5', surveyName:'Name 5', assignedon:'29-04-24', status: 'Attempted' },
+    {surveyId:'6', surveyName:'Name 6', assignedon:'29-04-24', status: 'Not attempted' },
+    {surveyId:'7', surveyName:'Name 6', assignedon:'29-04-24', status: 'Attempted' },
+    {surveyId:'8', surveyName:'Name 7', assignedon:'29-04-24', status: 'Not attempted' },
+    {surveyId:'9', surveyName:'Name 8', assignedon:'29-04-24', status: 'Not attempted' },
+    {surveyId:'10', surveyName:'Name 9', assignedon:'29-04-24', status: 'Attempted' },
+    {surveyId:'11', surveyName:'Name 10', assignedon:'29-04-24', status: 'Not attempted' },
   ];
+
+
+  getSurveysByStatus(status:any){
+
+  }
 }
