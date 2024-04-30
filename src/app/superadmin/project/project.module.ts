cfd3c2974cec2911c87a65aba0f73cae4e76d8a2
gauravdev30 <gauravdagdu@gmail.com>
2024-04-30 22:19:29 +0530

    update

--- src/app/superadmin/project/project.module.ts
+++ src/app/superadmin/project/project.module.ts
@@ -50,6 +50,7 @@ import { PhaseoneComponent } from './Components/dashboard/phaseone/phaseone.comp
 import { PhasetwoComponent } from './Components/dashboard/phasetwo/phasetwo.component';
 import { CreateUserComponent } from './Components/project-admin/create-user/create-user.component';
 import { CreateGroupComponent } from './Components/meetings/create-group/create-group.component';
+import { ScheduleComponent } from './Components/meetings/schedule/schedule.component';
 
 @NgModule({
   declarations: [
@@ -81,6 +82,7 @@ import { CreateGroupComponent } from './Components/meetings/create-group/create-
     PhasetwoComponent,
     CreateUserComponent,
     CreateGroupComponent,
+    ScheduleComponent,
     
   ],
   imports: [
