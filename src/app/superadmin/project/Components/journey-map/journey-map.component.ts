7aa9574773c51771dca7d51a6a5e4145176e24c2
priyanka_bagul <tablabstechnologies@gmail.com>
2024-05-13 12:45:31 +0530

    update

---
+++ src/app/superadmin/project/Components/journey-map/journey-map.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journey-map',
+  templateUrl: './journey-map.component.html',
+  styleUrl: './journey-map.component.css'
+})
+export class JourneyMapComponent {
+
+}
