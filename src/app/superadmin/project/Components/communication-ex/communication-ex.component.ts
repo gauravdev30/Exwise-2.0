789a1ea21324a5f0d945980cf2649d343989771f
gauravdev30 <gauravdagdu@gmail.com>
2024-05-14 21:52:17 +0530

    update

--- src/app/superadmin/project/Components/communication-ex/communication-ex.component.ts
+++ src/app/superadmin/project/Components/communication-ex/communication-ex.component.ts
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrl: './communication-ex.component.css'
 })
 export class CommunicationExComponent {
+  messages = [
+    { text: 'Hello and thank you for visiting MDBootstrap. Please click the video below.', type: 'received' },
+    { text: 'Thank you, I really like your product.', type: 'sent' }
+  ];
+  newMessage = '';
 
+  sendMessage(event?: KeyboardEvent) {
+    if (event) {
+      event.preventDefault(); // Prevents the default action (e.g., form submission)
+    }
+
+    if (this.newMessage.trim() !== '') {
+      this.messages.push({ text: this.newMessage, type: 'sent' });
+      this.newMessage = '';
+    }
+  }
 }
