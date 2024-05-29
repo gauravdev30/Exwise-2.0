6ecedfd6477d976ada9c3d629e83c5cb91343b1c
Bhaskar Roy Chowdhury <bhaskaroychowdhury@gmail.com>
2024-05-29 15:26:31 +0530

    Bug fixes and css changes

--- src/app/client-employee/pages/profile/profile.component.ts
+++ src/app/client-employee/pages/profile/profile.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
-export class ProfileComponent {
-
+export class ProfileComponent implements OnInit {
+  profileInfo: any;
+  ngOnInit(): void {
+    this.profileInfo = JSON.parse(
+      sessionStorage.getItem('currentLoggedInUserData')!
+    );
+    console.log(this.profileInfo);
+  }
 }
