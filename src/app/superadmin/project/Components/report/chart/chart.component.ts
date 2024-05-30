18f608fa2bc17b6bce56ac57e8f257fd16e66b5b
priyanka_bagul <tablabstechnologies@gmail.com>
2024-05-30 17:29:19 +0530

    updareDashboard

--- src/app/superadmin/project/Components/report/chart/chart.component.ts
+++ src/app/superadmin/project/Components/report/chart/chart.component.ts
@@ -1,5 +1,7 @@
 import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
-import {Chart, Point} from "chart.js";
+import { ChartDataset } from 'chart.js';
+import {Chart, ChartConfiguration, ChartData, ChartEvent, ChartOptions, ChartType, Point} from "chart.js";
+import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
@@ -7,5 +9,86 @@ import {Chart, Point} from "chart.js";
   styleUrl: './chart.component.css'
 })
 export class ChartComponent {
-show:any=1;
+  // Doughnut
+  public doughnutChartLabels: string[] = [
+    'Download Sales',
+    'In-Store Sales',
+    'Mail-Order Sales',
+  ];
+  public doughnutChartData: ChartData<'doughnut'> = {
+    labels: this.doughnutChartLabels,
+    datasets: [
+      { data: [350, 450, 100] },
+     
+    ],
+  };
+  public doughnutChartType: ChartType = 'doughnut';
+  lineChartData: any;
+  newLabel: any;
+
+  // events
+  public chartClicked({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+  public chartHovered({
+    event,
+    active,
+  }: {
+    event: ChartEvent;
+    active: object[];
+  }): void {
+    console.log(event, active);
+  }
+
+
+  public barChartOptions: ChartConfiguration['options'] = {
+    elements: {
+      line: {
+        tension: 0.1,
+      },
+    },
+    // We use these empty structures as placeholders for dynamic theming.
+    scales: {
+      x: {},
+      y: {
+        min: 10,
+      },
+    },
+    plugins: {
+      legend: { display: true },
+    },
+  };
+  public barChartLabels: string[] = [
+    '2006',
+    '2007',
+    '2008',
+    '2009',
+    '2010',
+    '2011',
+    '2012',
+  ];
+  public barChartType: ChartType = 'bar';
+
+  public barChartData: ChartData<'bar'> = {
+    labels: this.barChartLabels,
+    datasets: [
+      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+    ],
+  };
+
+
+
+  public randomize(): void {
+    this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
+  }
+
+
 }
