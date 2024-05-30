88c710356b91e78ef781486e4377840209f6eb07
Bhaskar Roy Chowdhury <bhaskaroychowdhury@gmail.com>
2024-05-30 20:22:02 +0530

    Fixed graph layout

--- src/app/superadmin/project/Components/report/chart/chart.component.ts
+++ src/app/superadmin/project/Components/report/chart/chart.component.ts
@@ -1,12 +1,26 @@
-import {AfterViewInit, Component, ElementRef, Input, ViewChild} from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  Input,
+  ViewChild,
+} from '@angular/core';
 import { ChartDataset } from 'chart.js';
-import {Chart, ChartConfiguration, ChartData, ChartEvent, ChartOptions, ChartType, Point} from "chart.js";
+import {
+  Chart,
+  ChartConfiguration,
+  ChartData,
+  ChartEvent,
+  ChartOptions,
+  ChartType,
+  Point,
+} from 'chart.js';
 import { BaseChartDirective } from 'ng2-charts';
 
 @Component({
   selector: 'app-chart',
   templateUrl: './chart.component.html',
-  styleUrl: './chart.component.css'
+  styleUrl: './chart.component.css',
 })
 export class ChartComponent {
   // Doughnut
@@ -18,78 +32,77 @@ export class ChartComponent {
   public doughnutChartData: ChartData<'doughnut'> = {
     labels: this.doughnutChartLabels,
     datasets: [
-      { data: [350, 450, 100] },
-     
+      {
+        data: [350, 450, 100],
+      },
     ],
   };
   public doughnutChartType: ChartType = 'doughnut';
   lineChartData: any;
   newLabel: any;
 
+  barChart: any = [];
+  lineChart: any = [];
+
   // events
-  public chartClicked({
-    event,
-    active,
-  }: {
-    event: ChartEvent;
-    active: object[];
-  }): void {
-    console.log(event, active);
-  }
+  // public chartClicked({
+  //   event,
+  //   active,
+  // }: {
+  //   event: ChartEvent;
+  //   active: object[];
+  // }): void {
+  //   console.log(event, active);
+  // }
 
-  public chartHovered({
-    event,
-    active,
-  }: {
-    event: ChartEvent;
-    active: object[];
-  }): void {
-    console.log(event, active);
-  }
+  // public chartHovered({
+  //   event,
+  //   active,
+  // }: {
+  //   event: ChartEvent;
+  //   active: object[];
+  // }): void {
+  //   console.log(event, active);
+  // }
 
+  // public barChartOptions: ChartConfiguration['options'] = {
+  //   elements: {
+  //     line: {
+  //       tension: 0.1,
+  //     },
+  //   },
+  //   scales: {
+  //     x: {},
+  //     y: {
+  //       min: 10,
+  //     },
+  //   },
+  //   plugins: {
+  //     legend: { display: true },
+  //   },
+  // };
 
-  public barChartOptions: ChartConfiguration['options'] = {
-    elements: {
-      line: {
-        tension: 0.1,
-      },
-    },
-    // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10,
-      },
-    },
-    plugins: {
-      legend: { display: true },
-    },
-  };
-  public barChartLabels: string[] = [
-    '2006',
-    '2007',
-    '2008',
-    '2009',
-    '2010',
-    '2011',
-    '2012',
-  ];
-  public barChartType: ChartType = 'bar';
+  // public barChartType: ChartType = 'bar';
 
-  public barChartData: ChartData<'bar'> = {
-    labels: this.barChartLabels,
-    datasets: [
-      { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-      { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-    ],
-  };
+  // public barChartData: ChartData<'bar'> = {
+  //   labels: [
+  //     '2006',
+  //     '2007',
+  //     '2008',
+  //     '2009',
+  //     '2010',
+  //     '2011',
+  //     '2012',
+  //   ],
+  //   datasets: [
+  //     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+  //     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
+  //   ],
+  // };
 
-
-
-  public randomize(): void {
-    this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
-  }
-
+  // public randomize(): void {
+  //   this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
+  // }
 
   // second graph
 
@@ -107,9 +120,9 @@ export class ChartComponent {
         label: 'Series B',
         borderColor: 'rgba(54,162,235,1)',
         backgroundColor: 'rgba(54,162,235,0.2)',
-      }
+      },
     ],
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July']
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
   };
 
   public lineChartOptions: ChartOptions = {
@@ -121,24 +134,69 @@ export class ChartComponent {
     plugins: {
       title: {
         display: true,
-        text: 'Chart.js Line Chart - External Tooltips'
+        text: 'Chart.js Line Chart - External Tooltips',
       },
       tooltip: {
         enabled: false,
         position: 'nearest',
-        external: this.externalTooltipHandler
-      }
-    }
+        external: this.externalTooltipHandler,
+      },
+    },
   };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.barChart = new Chart('barChartCanvas', {
+      type: 'bar',
+      data: {
+        labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+        datasets: [
+          {
+            data: [1000, 200, 300, 400, 500, 600, 70],
+            label: 'Series A',
+            borderColor: 'rgba(255,99,132,1)',
+            backgroundColor: '#2155a3',
+            maxBarThickness: 15,
+            borderRadius: 10,
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    this.lineChart = new Chart('lineChartCanvas', {
+      type: 'line',
+      data: {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            data: [65, 59, 80, 81, 56, 55, 40],
+            label: 'Series A',
+            borderColor: 'rgba(255,99,132,1)',
+            backgroundColor: 'rgba(255,99,132,0.2)',
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+  }
 
   externalTooltipHandler(context: any) {
     // Implement your custom tooltip logic here
     const { chart, tooltip } = context;
     // Custom tooltip code
   }
-
 }
