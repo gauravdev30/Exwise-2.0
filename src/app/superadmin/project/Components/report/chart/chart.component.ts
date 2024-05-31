7e41c8417f543680b793ee93f903826ca8bddb9b
gauravdev30 <gauravdagdu@gmail.com>
2024-05-31 13:47:42 +0530

    Update

--- src/app/superadmin/project/Components/report/chart/chart.component.ts
+++ src/app/superadmin/project/Components/report/chart/chart.component.ts
@@ -43,122 +43,22 @@ export class ChartComponent {
 
   barChart: any = [];
   lineChart: any = [];
-
-  // events
-  // public chartClicked({
-  //   event,
-  //   active,
-  // }: {
-  //   event: ChartEvent;
-  //   active: object[];
-  // }): void {
-  //   console.log(event, active);
-  // }
-
-  // public chartHovered({
-  //   event,
-  //   active,
-  // }: {
-  //   event: ChartEvent;
-  //   active: object[];
-  // }): void {
-  //   console.log(event, active);
-  // }
-
-  // public barChartOptions: ChartConfiguration['options'] = {
-  //   elements: {
-  //     line: {
-  //       tension: 0.1,
-  //     },
-  //   },
-  //   scales: {
-  //     x: {},
-  //     y: {
-  //       min: 10,
-  //     },
-  //   },
-  //   plugins: {
-  //     legend: { display: true },
-  //   },
-  // };
-
-  // public barChartType: ChartType = 'bar';
-
-  // public barChartData: ChartData<'bar'> = {
-  //   labels: [
-  //     '2006',
-  //     '2007',
-  //     '2008',
-  //     '2009',
-  //     '2010',
-  //     '2011',
-  //     '2012',
-  //   ],
-  //   datasets: [
-  //     { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
-  //     { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' },
-  //   ],
-  // };
-
-  // public randomize(): void {
-  //   this.barChartType = this.barChartType === 'bar' ? 'line' : 'bar';
-  // }
-
-  // second graph
-
-  public lineChartType: ChartType = 'line';
-  public lineChartData2: ChartConfiguration['data'] = {
-    datasets: [
-      {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        borderColor: 'rgba(255,99,132,1)',
-        backgroundColor: 'rgba(255,99,132,0.2)',
-      },
-      {
-        data: [28, 48, 40, 19, 86, 27, 90],
-        label: 'Series B',
-        borderColor: 'rgba(54,162,235,1)',
-        backgroundColor: 'rgba(54,162,235,0.2)',
-      },
-    ],
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-  };
-
-  public lineChartOptions: ChartOptions = {
-    responsive: true,
-    interaction: {
-      mode: 'index',
-      intersect: false,
-    },
-    plugins: {
-      title: {
-        display: true,
-        text: 'Chart.js Line Chart - External Tooltips',
-      },
-      tooltip: {
-        enabled: false,
-        position: 'nearest',
-        external: this.externalTooltipHandler,
-      },
-    },
-  };
-
+  doughnutChart:any = [];
+ 
   constructor() {}
 
   ngOnInit(): void {
     this.barChart = new Chart('barChartCanvas', {
-      type: 'bar',
+      type: 'line',
       data: {
         labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
         datasets: [
           {
-            data: [1000, 200, 300, 400, 500, 600, 70],
+            data: [200, 400, 300, 400, 500, 600, 70],
             label: 'Series A',
-            borderColor: 'rgba(255,99,132,1)',
-            backgroundColor: '#2155a3',
-            maxBarThickness: 15,
-            borderRadius: 10,
+            borderColor: "#2155a3",
+            // backgroundColor: '#2155a3',
+            fill:true
           },
         ],
       },
@@ -181,6 +81,42 @@ export class ChartComponent {
             label: 'Series A',
             borderColor: 'rgba(255,99,132,1)',
             backgroundColor: 'rgba(255,99,132,0.2)',
+          },
+        ],
+      },
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true,
+          },
+        },
+      },
+    });
+
+    this.doughnutChart =new Chart('doughnutChartCanvas', {
+      type: 'doughnut',
+      data: {
+        labels: ['January', 'February', 'March', 'April'],
+        datasets: [
+          {
+            data: [2],
+            label: 'January',
+            backgroundColor: '#2155a3',
+          },
+          {
+            data: [3],
+            label: 'February',
+            backgroundColor: '#70c4fe',
+          },
+          {
+            data: [4],
+            label: 'March',
+            backgroundColor: '#2980b9',
+          },
+          {
+            data: [1],
+            label: 'April',
+            backgroundColor: '#069de0',
           },
         ],
       },
