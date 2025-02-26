6e56361bec46ed7bd7bde31b5ec0627633b4a7c3
gauravdev30 <152850681+gauravdev30@users.noreply.github.com>
2025-02-25 23:31:07 -0800

    update baseurl

--- src/environment/enviorment.prod.ts
+++ src/environment/enviorment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl:'http://ec2-13-127-136-153.ap-south-1.compute.amazonaws.com:8080/test_exwise/api/',
-    baseUrl2:'http://ec2-13-127-136-153.ap-south-1.compute.amazonaws.com:8080/test_exwise/api/',
+    baseUrl:'http://ec2-13-127-136-153.ap-south-1.compute.amazonaws.com:8080/production_exwise/api/',
+    baseUrl2:'http://ec2-13-127-136-153.ap-south-1.compute.amazonaws.com:8080/production_exwise/api/',
     // excelFormatFileUrl: 'https://tablabs-images.s3.ap-south-1.amazonaws.com/1738139216686-userUploadFormat__revised_Raja.xlsx',
     excelFileFormatUrlForPeopleMatrix : 'https://tablabs-images.s3.ap-south-1.amazonaws.com/1733486829511-people_matrix.xlsx',
 
